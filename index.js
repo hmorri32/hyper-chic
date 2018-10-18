011aacdd354e65312715b05853c252e7a71338ee
Hugh Morrison <hmorri32@gmail.com>
2018-10-18 09:29:39 -0600

    hand rolls sweet split pane opacity situation and ups version number

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
-const backgroundColor = '#000000';
+const backgroundColor = '#323E4D';
 const foregroundColor = '#CDD2E9';
 const cursorColor = '#2C85F7';
 const borderColor = '#323E4D';
 const colors = {
-  black        : backgroundColor,
-  red          : '#fac14f',
-  green        : '#90c2e7',
-  yellow       : '#ddb22f',
-  blue         : '#56aa59',
-  magenta      : '#c0a8d0',
-  cyan         : '#f8ccde',
-  white        : foregroundColor,
-  lightBlack   : '#546386',
-  lightRed     : '#fac14f',
-  lightGreen   : '#90c2e7',
-  lightYellow  : '#FFB68E',
-  lightBlue    : '#56aa59',
-  lightMagenta : '#c0a8d0',
-  lightCyan    : '#f8ccde',
-  lightWhite   : '#fefbee',
+  black: backgroundColor,
+  red: '#fac14f',
+  green: '#90c2e7',
+  yellow: '#ddb22f',
+  blue: '#56aa59',
+  magenta: '#c0a8d0',
+  cyan: '#f8ccde',
+  white: foregroundColor,
+  lightBlack: '#546386',
+  lightRed: '#fac14f',
+  lightGreen: '#90c2e7',
+  lightYellow: '#FFB68E',
+  lightBlue: '#56aa59',
+  lightMagenta: '#c0a8d0',
+  lightCyan: '#f8ccde',
+  lightWhite: '#fefbee',
+};
+
+exports.decorateBrowserOptions = opts => {
+  opts.transparent = false;
+  return opts;
 };
 
 exports.decorateConfig = config => {
@@ -28,16 +33,22 @@ exports.decorateConfig = config => {
     borderColor,
     colors,
     selectionColor: 'rgba(224, 239, 254,0.3)',
-    cursorColor:    config.cursorColor || cursorColor,
-    cursorShape:    config.cursorShape || 'BLOCK',
-    fontSize:       config.fontSize    || 12,
-    fontFamily:     config.fontFamily  || '"Meslo LG S for Powerline"',
-    css: `
-      ${config.css || ''}
-      .terminal, .term_fit:not(.term_term) { opacity: 1 !important; }
-      ::selection {
-          background: #9198A2 !important;
+    cursorColor: config.cursorColor || cursorColor,
+    cursorShape: config.cursorShape || 'BLOCK',
+    fontSize: config.fontSize || 12,
+    fontFamily: config.fontFamily || '"Meslo LG S for Powerline"',
+    css: (config.css += `
+      .term_fit:not(.term_term) {
+        opacity: 0.5 !important;
       }
-    `,
+      .terminal, .term_fit.term_active {
+        opacity: 1 !important;
+        transition: opacity 0.12s ease-in-out;
+        will-change: opacity;
+      }
+      .term_fit.term_wrapper {
+        opacity: 1 !important;
+      }
+    `),
   });
 };
